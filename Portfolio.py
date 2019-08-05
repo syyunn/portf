cd4e1e4d41e41ca350d056dfd1da20f78b6ab666
Zachary <syyun@snu.ac.kr>
2019-08-05 22:59:29 +0900

    Make Asset class

--- Portfolio.py
+++ Portfolio.py
@@ -1,8 +1,8 @@
 class Portfolio:
     def __init__(self,
-                 kinds,
+                 asset_kinds,
                  values):
-        for key, val in zip(kinds, values):
+        for key, val in zip(asset_kinds, values):
             self.portf = dict()
             self.portf[key] = val
 
@@ -12,6 +12,32 @@ class Portfolio:
         self.portf = self.portf + {key: value}
 
 
+class Asset:
+    def __init__(self,
+                 name):
+        self.name = name
+        self.total = 0
+        self.volume = 0
+        self.history = []
+        self.unit = None
+        if name == "USD":
+            self.unit = "USD"
+        elif name == "GOLD":
+            self.unit = "g"
+
+    def add_volume(self,
+                   volumes):
+        assert isinstance(volumes, list), "volumes must be list instance."
+
+        self.history += volumes
+        self.total = sum(self.history)
+
+
 if __name__ == "__main__":
-    kinds = []
+    usd = Asset("USD")
+    history = [1700, 830, 826, 818.5, 1650, 818.5, 1637.6, 1636.8]
+    usd.add_volume(history)
+    print(usd.total)
+
+
     pass
